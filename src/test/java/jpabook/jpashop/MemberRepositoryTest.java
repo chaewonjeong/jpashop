51ba8b9cd3d4cd70864201581826b07952e12a86
chaewonjeong <chaewonjeong.dev@gmail.com>
2024-07-01 21:21:11 +0900

    first commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    publi
+}
